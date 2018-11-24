9d3c64107e786945b313aaad8b7868c6f02eff1e
Piotr Czapka <pczapka@wp.pl>
2018-11-24 22:38:49 +0100

    Added evaluation tests

--- ChessCoreEngine.Tests/EvaluationTests.cs
+++ ChessCoreEngine.Tests/EvaluationTests.cs
@@ -18,6 +18,7 @@ namespace ChessCoreEngine.Tests
 
         }
 
+        #region PieceScoreEvaluationTests
         [TestCase(11, true, 150, 3, 60)]
         [TestCase(12, true, 150, 4, 60)]
         [TestCase(13, true, 150, 5, 60)]
@@ -165,6 +166,27 @@ namespace ChessCoreEngine.Tests
             movedQueenNotEndgameScore.Should().Be(975-10);
             notMovedQueenEndgameScore.Should().Be(975);
             notMovedQueenNotEndgameScore.Should().Be(975);
+        }
+
+        #endregion
+
+        [TestCase("r4k1r/8/4q3/8/8/8/4NNB1/R1B3K1 b - - 0 1", -195)]
+        [TestCase("r5k1/5pp1/1pNp2bp/1P1P4/2q5/1R3P1P/3r2PK/3Q4 w - - 0 40", -495)]
+        [TestCase("rn1r2k1/pp2bppp/6q1/3pP1N1/8/2N1B3/PP1Q1PbP/R4RK1 w - - 0 16", -57)]
+        [TestCase("r2r2k1/2q1bppp/2p1p3/pp1n1b2/2nP4/2PNBN1P/PP2BPP1/R1Q2RK1 b - - 5 17", 10)]
+        [TestCase("r2q1k2/3p1p2/p1p5/1p6/3P3Q/2P2PP1/PP3P2/R5K1 b - - 0 27", 271)]
+        [TestCase("rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2", 75)]
+        [TestCase("rnbqkbnr/1p1p1ppp/p7/4p3/4P3/5N2/PPP2PPP/RNBQKB1R b KQkq - 1 5", 70)]
+        [TestCase("r5k1/2R4q/p6p/8/3P2P1/4P3/2P4K/8 w - - 0 35", -680)]
+        [TestCase("8/8/8/1p1p1kpp/3P4/1r3PPP/1P1R1K2/8 b - - 0 43", 150)]
+        [TestCase("r1bqkbnr/1ppp1ppp/p1n5/4p3/B3P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 1 4", 20)]
+        public void BoardEvaluationTest(string fen, int expectedScore)
+        {
+            var board = Board.CreateBoardFromFen(fen);
+            var score = Evaluation.EvaluateBoardScore(board.GetEvaluationParameters(), board);
+
+            score.Should().Be(expectedScore);
+
         }
     }
 }
