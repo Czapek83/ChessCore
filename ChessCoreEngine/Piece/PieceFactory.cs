4f9b75384fe31009d276eb9669f4a169697788af
Piotr Czapka <pczapka@wp.pl>
2018-11-24 15:09:25 +0100

    Introduced PieceFactory

---
+++ ChessCoreEngine/Piece/PieceFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessEngine.Engine
+{
+    public static class PieceFactory
+    {
+        public static Piece CreatePieceByTypeAndColor(ChessPieceType chesspieceType,
+            ChessPieceColor chessPieceColor)
+        {
+            switch (chesspieceType)
+            {
+                case ChessPieceType.Bishop:
+                    return new Bishop(chessPieceColor);
+                case ChessPieceType.Knight:
+                    return new Knight(chessPieceColor);
+                case ChessPieceType.Rook:
+                    return new Rook(chessPieceColor);
+                case ChessPieceType.Queen:
+                    return new Queen(chessPieceColor);
+                case ChessPieceType.King:
+                    return new King(chessPieceColor);
+                case ChessPieceType.Pawn:
+                    return new Pawn(chessPieceColor);
+                default: throw new ArgumentException($"Invalid chessPieceType {chesspieceType}");
+            }
+        }
+    }
+}
