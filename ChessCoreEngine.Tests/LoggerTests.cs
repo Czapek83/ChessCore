3fd27a89f0c8e72e22713799726edee853749582
Piotr Czapka <pczapka@wp.pl>
2018-11-29 22:09:36 +0100

    Added logger class and introduced tests for that

---
+++ ChessCoreEngine.Tests/LoggerTests.cs
@@ -0,0 +1,42 @@
+﻿using AutoFixture;
+using ChessEngine.Engine;
+using Moq;
+using Moq.Protected;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessCoreEngine.Tests
+{
+    [TestFixture]
+    public class LoggerTests
+    {
+        private Fixture fixture = new Fixture();
+
+
+        [TestCase(LogLevel.Debug, 1, 1, 1)]
+        [TestCase(LogLevel.Info, 0, 1, 1)]
+        [TestCase(LogLevel.Error, 0, 0, 1)]
+        [TestCase(LogLevel.NoLog, 0, 0, 0)]
+        public void LoggerTest(LogLevel logLevel, int logDebugTimes, int logInfoTimes,
+            int logErrorTimes)
+        {
+            var mock = new Mock<LoggerBase>(logLevel);
+            mock.Protected().Setup("LogDebugImpl", ItExpr.IsAny<string>());
+            mock.Protected().Setup("LogInfoImpl", ItExpr.IsAny<string>());
+            mock.Protected().Setup("LogErrorImpl", ItExpr.IsAny<string>());
+            var systemUnderTests = mock.Object;
+
+            systemUnderTests.LogDebug(fixture.Create<string>());
+            systemUnderTests.LogInfo(fixture.Create<string>());
+            systemUnderTests.LogError(fixture.Create<string>());
+
+            mock.Protected().Verify("LogDebugImpl", Times.Exactly(logDebugTimes), ItExpr.IsAny<string>());
+            mock.Protected().Verify("LogInfoImpl", Times.Exactly(logInfoTimes), ItExpr.IsAny<string>());
+            mock.Protected().Verify("LogErrorImpl", Times.Exactly(logErrorTimes), ItExpr.IsAny<string>());
+
+        }
+
+    }
+}
---
+++ ChessCoreEngine/Loggers/LoggerBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessEngine.Engine
+{
+    public enum LogLevel
+    {
+        Debug,
+        Info,
+        Error,
+        NoLog
+    }
+
+    public abstract class LoggerBase
+    {
+        protected readonly LogLevel _logLevel;
+
+        protected LoggerBase(LogLevel logLevel)
+        {
+            _logLevel = logLevel;
+        }
+
+        public void LogDebug(string message)
+        {
+            if (_logLevel == LogLevel.Debug)
+                LogDebugImpl(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            if (_logLevel == LogLevel.Debug || _logLevel == LogLevel.Info)
+                LogInfoImpl(message);
+        }
+
+        public void LogError(string message)
+        {
+            if (_logLevel != LogLevel.NoLog)
+                LogErrorImpl(message);
+        }
+
+        protected abstract void LogDebugImpl(string message);
+        protected abstract void LogInfoImpl(string message);
+        protected abstract void LogErrorImpl(string message);
+    }
+}
